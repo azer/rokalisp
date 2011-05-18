bb660040cf0a46f4024d58e693e36327ecc98fbd
E.Azer Koçulu <azer@kodfabrik.com>
2011-05-18 06:33:28 +0300

    first entry

--- src/lisp.js
+++ src/lisp.js
@@ -1,50 +1,9 @@
-var SExpression = require('./types/sexpression').SExpression;
-    HashTable = require('./types/hashtable').HashTable,
-    Symbol = require('./types/symbol').Symbol,
-    pop = require('./utils').pop,
-    match = require('./utils').match,
-    logger = require('./logger'),
-    library = require('./library');
-
-/**
- * Token Table
- */
-var tokenTable = exports.tokenTable = [];
-
-/**
- * Global Scope
- */
-var globalScope = new HashTable(library);
-globalScope.parent = new HashTable(typeof window === 'undefined' && global || window);
-
-/**
- * Create new function extending an abstract token type.
- */
-var createType = exports.createType = function createType(pattern,callback){
-  return function(tokens){
-    var matching = match(pattern,tokens[0]);
-    if(matching){
-      tokens.splice(0,1);
-      callback(tokens,matching);
-    }
-  }
-}
-
-/**
- * Evaluate an expression in an environment.
- */
-var evaluate = exports.evaluate = function evaluate(exp,env){
-  !env && ( env = globalScope );
-  
-  logger.debug('Evaluating expression:',exp,' with environment:',env);
-
-  return exp.evaluate(env);
-}
+var logger = require('./logger');
 
 /**
  * Read and parse given sequence of tokens.
  */
-var read = exports.read = function read(tokens){
+function read(tokens){
   if(tokens.length == 0) throw new Error('Unexpected EOF while reading');
 
   logger.debug('Reading tokens:',tokens);
@@ -63,7 +22,7 @@ var read = exports.read = function read(tokens){
 /**
  * Convert a string into a list of tokens
  */
-var tokenize = exports.tokenize = function tokenize(input){
+function tokenize(input){
   
   logger.debug('starting to tokenize');
 
@@ -86,110 +45,3 @@ var tokenize = exports.tokenize = function tokenize(input){
 };
 
 var tokenizer = /\s*([()]|[\{\}]|[\[\]]|"(?:[\\].|[^\\"])*"|;.*|[^\s'"`,;\[\]\{\}()]*)([\s\S]*)/;
-
-/**
- * undefined
- */
-tokenTable.push(createType(/^undefined$/,function(tokens,matching){
-  throw undefined;
-}));
-
-/**
- * null
- */
-tokenTable.push(createType(/^null$/),function(){
-  return null;
-});
-
-/**
- * Numbers
- * > 314
- * > 3.14
- */
-tokenTable.push(createType(/(^\d[\d\.e\+]*)$/,function(tokens,matching){
-  throw Number(matching.input);
-}));
-
-/**
- * Strings
- * > "hello world"
- */
-tokenTable.push(createType(/^"(.*)"$/,function(tokens,matching){
-  throw matching[0];
-}));
-
-/**
- * Booleans
- * > #t
- * > #f
- */
-tokenTable.push(createType(/#(t|f)/,function(tokens,matching){
-  throw matching[0] == 't';
-}));
-
-/**
- * S-Expressions
- * > (....)
- */
-tokenTable.push(createType(/^\(/,function(tokens,matching){
-  var tree = new SExpression();
-
-  while(tokens[0] != ')'){
-    tree.context.push( read(tokens) );
-  };
-  pop(tokens,0);
-
-  throw tree;
-}));
-
-tokenTable.push(createType(/^\)/,function(tokens,matching){
-  throw new Error('Unexpected ")"');
-}));
-
-/**
- * Arrays
- * > [....]
- */
-tokenTable.push(createType(/^\[/,function(tokens,matching){
-  var list = [];
-
-  while(tokens[0] != ']'){
-    list.push( read(tokens) );
-  };
-  pop(tokens,0);
-
-  throw list;
-}));
-
-tokenTable.push(createType(/^\]/,function(tokens,matching){
-  throw new Error('Unexpected "]"');
-}));
-
-/**
- * Hash Tables
- * > { ..... }
- */
-tokenTable.push(createType(/^\{/,function(tokens,matching){
-
-
-  var token = pop(tokens,0);
-  while(token != '}'){
-    if(token.substring(0,1)==':'){
-      ht[token.substring(1)] = read(pop(tokens,0));
-    }
-  };
-
-  throw ht;
-}));
-
-tokenTable.push(createType(/^\}/,function(tokens,matching){
-  throw new Error('Unexpected "}"');
-}));
-
-/**
- * Symbols
- * > foobar
- */
-tokenTable.push(createType(/([\w\*\+\!\-_\?<>]+)/,function(tokens,matching){
-  throw new Symbol(matching[0]);
-}));
